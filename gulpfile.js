a9e8b9fb17fb7815bd175d3aae2f775a2b5ecc99
salah ahmed <salah93@crosscompute.com>
2018-07-24 23:34:33 -0400

    Use gulp for easier development
    refer to https://css-tricks.com/gulp-for-beginners/ for tutorial

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+
+gulp.task('default', ['watch'])
+
+/* gulp.task('watch', ['array', 'of', 'tasks',
+ *        'to', 'complete','before', 'watch'], function (){
+ *      ...
+ *  })
+ */
+gulp.task('watch', ['browserSync'], function() {
+  // gulp.watch('files-to-watch', ['tasks', 'to', 'run']);
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('src/js/**/*.js', browserSync.reload);
+  gulp.watch('src/css/**/*.css', browserSync.reload);
+  // Other watchers
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: '.',
+    },
+  });
+});
