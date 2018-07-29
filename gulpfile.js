8e94804064284d295648926dd8e053ce661b4473
Rodrigo Guarachi <rmGuarachi@gmail.com>
2018-07-29 18:43:20 -0400

    homepage colors, and jumbo done

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('default', ['watch'])
  */
 gulp.task('watch', ['browserSync'], function() {
   // gulp.watch('files-to-watch', ['tasks', 'to', 'run']);
-  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('src/*.html', browserSync.reload);
   gulp.watch('src/js/**/*.js', browserSync.reload);
   gulp.watch('src/css/**/*.css', browserSync.reload);
   // Other watchers
@@ -20,7 +20,7 @@ gulp.task('watch', ['browserSync'], function() {
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: '.',
+      baseDir: './src/',
     },
   });
 });
